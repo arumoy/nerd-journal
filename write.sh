f74a4de89c160c1befccd128dc8cd504fe6ff855
Arumoy Chakraborty <arumoy0@gmail.com>
2019-12-17 15:51:18 +0530

    adds a timestamp to end instead of overwriting the file - bash support

--- write.sh
+++ write.sh
@@ -2,5 +2,5 @@
 today_folder=`date "+%d-%m-%Y"`
 post_header="# `date "+%d %B %Y, %I:%M:%S %p"`"
 mkdir $today_folder
-echo $post_header > $today_folder/README.md
+echo $post_header >> $today_folder/README.md
 code $today_folder/README.md
