4b927698ba13d7f4ec1399f8bd0b16a2997aaf1b
Arumoy Chakraborty <arumoy0@gmail.com>
2019-12-26 19:19:11 +0530

    changes folder format to more sortable one and drops batch file support

--- write.ps1
+++ write.ps1
@@ -1,4 +1,4 @@
-$today_folder = Get-Date -DisplayHint DateTime -Format dd-MM-yyyy
+$today_folder = Get-Date -DisplayHint DateTime -Format yyyy-MM-dd
 mkdir $today_folder | Out-Null
 $post_header = Get-Date -DisplayHint DateTime -Format "dd MMMM yyyy, hh:mm:ss tt"
 Add-Content -Path $today_folder\README.md -Value "# $post_header"
--- write.sh
+++ write.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-today_folder=`date "+%d-%m-%Y"`
+today_folder=`date "+%Y-%m-%d"`
 post_header="# `date "+%d %B %Y, %I:%M:%S %p"`"
 mkdir $today_folder
 echo $post_header >> $today_folder/README.md
