8b70add3527430c838de26b9a61adb2fbc5139f2
Arumoy Chakraborty <arumoy0@gmail.com>
2020-01-25 22:01:26 +0530

    changes the linux default editor to vim, code is shit in linux

--- write.sh
+++ write.sh
@@ -3,4 +3,4 @@ today_folder=`date "+%Y-%m-%d"`
 post_header="# `date "+%d %B %Y, %I:%M:%S %p"`"
 mkdir $today_folder
 echo $post_header >> $today_folder/README.md
-code $today_folder/README.md
+vim $today_folder/README.md
