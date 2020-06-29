d590d687437023679c6da2804f41288f8aa6dd26
Arumoy Chakraborty <arumoy0@gmail.com>
2020-06-29 10:53:56 +0530

    Powershell publish script publishes day only folder, not whole repo

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,5 @@
 $today = Get-Date -DisplayHint Date -Format "dd MMMM yyyy"
-git add .
+$today_folder = Get-Date -DisplayHint DateTime -Format yyyy-MM-dd
+git add $today_folder\*
 git commit -m "Journal entry for $today"
 git push -u origin master
